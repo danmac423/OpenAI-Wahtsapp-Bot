c6e3cc53acefd6e6b69d865b69261982ff4347f7
dmachnia <01178558@pw.edu.pl>
2024-11-15 00:09:35 +0100

    WB-16 Add basic tests to check if it works

--- src/test/java/whatsappbroski/whatsappbroski/WhatsAppBroskiApplicationTests.java
+++ src/test/java/whatsappbroski/whatsappbroski/WhatsAppBroskiApplicationTests.java
@@ -1,13 +1,22 @@
 package whatsappbroski.whatsappbroski;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import whatsappbroski.whatsappbroski.controller.HelloController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class WhatsAppBroskiApplicationTests {
 
+    @Autowired
+    private HelloController helloController;
+
     @Test
     void contextLoads() {
+        assertThat(helloController).isNotNull();
     }
 
 }
