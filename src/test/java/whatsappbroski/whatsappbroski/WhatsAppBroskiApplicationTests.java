443a235f18a88a01f02cfb5bfe9dcc330a045d29
dmachnia <01178558@pw.edu.pl>
2024-11-14 23:23:27 +0100

    WB-15 Add initial project structure

---
+++ src/test/java/whatsappbroski/whatsappbroski/WhatsAppBroskiApplicationTests.java
@@ -0,0 +1,13 @@
+package whatsappbroski.whatsappbroski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppBroskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
