3ac432ef3e5fbcd871bbf63b0ce3373772e3a215
dmachnia <01178558@pw.edu.pl>
2024-12-15 14:05:25 +0100

    WB-30 Change deployment script to export whatsapp secrets to docker image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-      echo "Usage: $0 <docker-username> <docker-token>"
+if [ $# -ne 5 ]; then
+      echo "Usage: $0 <docker-username> <docker-token> <whatsapp-api-ul> <whatsapp-api-token> <whatsapp-api-webhook-token>"
       exit 1
   fi
 
@@ -17,9 +17,12 @@ fi
 
 DOCKERHUB_USERNAME=$1
 DOCKERHUB_TOKEN=$2
+WHATSAPP_API_URL=$3
+WHATSAPP_API_TOKEN=$4
+WHATSAPP_API_WEBHOOK_TOKEN=$5
 
 set -ex
 
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin docker.io
 docker pull msj102/whatsappbroski:latest
-docker run -d -p "8080:8080" --name $CONTAINER_NAME --rm msj102/whatsappbroski
+docker run -d  -e WHATSAPP_API_URL="${WHATSAPP_API_URL}" -e WHATSAPP_API_TOKEN="${WHATSAPP_API_TOKEN}" -e WHATSAPP_API_WEBHOOK_TOKEN="${WHATSAPP_API_WEBHOOK_TOKEN}" -p "8080:8080" --name $CONTAINER_NAME --rm msj102/whatsappbroski
